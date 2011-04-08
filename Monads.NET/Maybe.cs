1eaea409521c1d0be70712934cc69ada6c459080
Erin Houston <erinhouston@gmail.com>
2011-04-07 20:59:22 -0600

    monads one

---
+++ Monads.NET/Maybe.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Monads.NET
+{
+    public static class Maybe
+    {
+        public static TInput If<TInput>(this TInput o, Func<TInput, bool> evaluator)
+            where TInput : class 
+        {
+            return (o == null) ? null : evaluator(o) ? o : null;
+        }
+
+        public static TInput Unless<TInput>(this TInput o, Func<TInput, bool> evaluator)
+               where TInput : class 
+        {
+            return (o == null) ? null : evaluator(o) ? null : o;
+        }
+
+        public static TResult With<TInput, TResult>(this TInput o, Func<TInput, TResult> evaluator)
+            where TResult : class
+            where TInput : class 
+        {
+            return (o == null) ? null : evaluator(o);
+        }
+
+        public static TResult With<TKey, TResult>(this Dictionary<TKey, TResult> o, TKey key)
+            where TResult : class 
+        {
+            return (o == null || !o.ContainsKey(key)) ? null : o[key];
+        }
+
+        public static TResult Return<TInput, TResult>(this TInput o, Func<TInput, TResult> evaluator, TResult failureValue) where TInput : class 
+        {
+            return (o == null) ? failureValue : evaluator(o);
+        }
+
+        public static TResult Return<TKey, TResult>(this Dictionary<TKey, TResult> o, TKey key, TResult failureValue) 
+        {
+            return (o == null || !o.ContainsKey(key)) ? failureValue : o[key];
+        }
+
+        public static TInput Do<TInput>(this TInput o, Action<TInput> action) where TInput : class 
+        {
+            if (o == null) return null;
+            action(o);
+            return o;
+        }
+    }
+}
---
+++ Monads.Net.Tests/MaybeTests.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Monads.NET;
+
+namespace Monads.Net.Tests
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class MaybeTests
+    {
+        public MaybeTests()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void With_on_An_Class_returns_value() 
+        {
+            var testValue = "Erin Was here";
+            var td = new TestableDummy
+            {
+                DummyString = testValue,
+                DummyValueType = 5
+            };
+
+            Assert.AreEqual(td.With(c => c.DummyString), testValue);
+        }       
+
+        [TestMethod]
+        public void With_will_return_Null_when_class_is_null() 
+        {
+            TestableDummy td = null;
+
+            Assert.IsNull(td.With(c => c.DummyString));
+        }
+
+        [TestMethod]
+        public void With_on_Dictionary_returns_Value() 
+        {
+            var testValue = "Erin Was here";
+            var testKey = "tiger";
+            var td = new TestableDummy
+            {
+                DummyString = testValue,
+                DummyValueType = 5,
+                DummyDictionary = new Dictionary<string,string>()
+            };
+            td.DummyDictionary[testKey] = testValue;
+
+            Assert.AreEqual(td.DummyDictionary.With(testKey), testValue);
+        }
+
+        [TestMethod]
+        public void With_on_Dictionary_Without_Key_Returns_Null() 
+        {
+            var td = new TestableDummy
+            {              
+                DummyDictionary = new Dictionary<string, string>()
+            };
+
+            Assert.IsNull(td.DummyDictionary.With("TestValueLookup"));
+        }
+
+        [TestMethod]
+        public void With_on_null_Dictionary_will_Return_Null() 
+        {
+            var td = new TestableDummy
+            {                
+                DummyValueType = 5,
+            };
+
+            Assert.IsNull(td.DummyDictionary.With("TestValueLookup"));
+        }
+
+        [TestMethod]
+        public void Return_on_a_class_returns_value () 
+        {
+            var testValue = "Erin Was here";
+            var td = new TestableDummy
+            {
+                DummyString = testValue,
+                DummyValueType = 5
+            };
+
+            Assert.AreEqual(td.Return(c => c.DummyString, "monkey"), testValue);
+        }
+
+        [TestMethod]
+        public void Return_on_a_class_returns_ValueType_value() 
+        {
+            
+            var td = new TestableDummy
+            {            
+                DummyValueType = 5
+            };
+            Assert.AreEqual(td.Return(c => c.DummyValueType, 8), 5);
+        }
+
+        [TestMethod]
+        public void Return_on_null_class_returns_failValue() 
+        {
+            var testValue = "Erin Was here";
+            var td = new TestableDummy
+            {           
+                DummyValueType = 5
+            };
+            Assert.AreNotEqual(td.Return(c => c.DummyString,"monkey"), testValue);
+        }
+
+        [TestMethod]
+        public void Return_on_a_class_returns_ValueType_failvalue() 
+        {
+
+            var td = new TestableDummy
+            {
+                DummyString = "hi ho hi ho"
+            };
+            Assert.AreNotEqual(td.Return(c => c.DummyValueType, 8), 5);
+        }
+
+        [TestMethod]
+        public void Return_on_Dictionary_will_return_Value()
+        {
+            var testValue = "Erin Was here";
+            var testKey = "tiger";
+            var td = new TestableDummy
+            {
+                DummyString = testValue,
+                DummyValueType = 5,
+                DummyDictionary = new Dictionary<string, string>()
+            };
+            td.DummyDictionary[testKey] = testValue;
+            Assert.AreEqual(td.DummyDictionary.Return(testKey, "notTestValue"), testValue);
+        }
+
+        [TestMethod]
+        public void Return_on_Dictionary_no_key_return_fail_value()
+        {
+            var testValue = "Erin Was here";
+            var testKey = "tiger";
+            var td = new TestableDummy
+            {
+                DummyString = testValue,
+                DummyValueType = 5,
+                DummyDictionary = new Dictionary<string, string>()
+            };
+            td.DummyDictionary[testKey] = testValue;
+            Assert.AreNotEqual(td.DummyDictionary.Return("Fire", "notTestValue"), testValue);
+        }
+
+    [TestMethod]
+        public void Return_on_null_dictionary_returns_null()
+    {
+        var testValue = "Erin Was here";
+        var testKey = "tiger";
+        var td = new TestableDummy
+        {
+            DummyString = testValue,
+            DummyValueType = 5            
+        };
+        Assert.AreNotEqual(td.DummyDictionary.Return(testKey, "notTestValue"), testValue);
+    }
+
+
+    }
+
+    public class TestableDummy
+    {
+        public string DummyString { get; set; }
+        public int DummyValueType { get; set; }
+        public Dictionary<string, string> DummyDictionary { get; set; }
+    }
+    
+}
