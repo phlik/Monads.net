7d25e64e0eeef4c9aaaffd99a9f8f2b7af584ab3
Erin Houston <erinhouston@gmail.com>
2011-04-20 10:43:37 -0600

    added some comments and functions to maby.cs

--- Monads.NET/Maybe.cs
+++ Monads.NET/Maybe.cs
@@ -48,5 +48,66 @@ namespace Monads.NET
             action(o);
             return o;
         }
+
+        public static TInput Recover<TInput>(this TInput o, Func<TInput> evaluator)
+            where TInput : class
+        {
+            return o ?? evaluator();
+        }
+
+        public static TResult Let<TInput, TResult>(this TInput o, Func<TInput, TResult> evaluator, TResult failValue)
+        {
+            return (o == null) ? failValue : evaluator(o);
+        }
+
+        public static Tuple<TInput, Exception> TryDo<TInput>(this TInput o, Action<TInput> evaluator)
+                where TInput : class
+        {
+            try
+            {
+                if (o == null)
+                    return Tuple.Create<TInput, Exception>(null, null);
+
+                evaluator(o);
+
+                return Tuple.Create<TInput, Exception>(o, null);
+            }
+            catch (Exception ex)
+            {
+                return Tuple.Create<TInput, Exception>(o, ex);
+            }
+        }
+
+        public static Tuple<TResult, Exception> TryLet<TInput, TResult>(this TInput o, Func<TInput, TResult> evaluator)
+            where TInput : class
+            where TResult : class
+        {
+            try
+            {
+                if (o == null)
+                    return Tuple.Create<TResult, Exception>(null, null);
+
+                return Tuple.Create<TResult, Exception>(evaluator(o), null);
+            }
+            catch (Exception ex)
+            {
+                return Tuple.Create<TResult, Exception>(null, ex);
+            }
+        }
+
+        public static TInput Handle<TInput>(this Tuple<TInput, Exception> input, Action<Exception> log)
+            where TInput : class
+        {
+            if (input.Item2 != null)
+                log(input.Item2);
+
+            return input.Item1;
+        }
+
+        public static TInput Ignore<TInput>(this Tuple<TInput, Exception> input)
+            where TInput : class
+        {
+            return input.Item1;
+        }
     }
 }
